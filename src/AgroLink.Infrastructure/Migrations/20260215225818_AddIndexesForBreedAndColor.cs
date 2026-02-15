2780a41579af8ba3d5eef7088ed44750cdb8a3e9
Jaime Alvarez <jaimealv994@gmail.com>
2026-02-15 17:00:49 -0600

    add breed and color index

---
+++ src/AgroLink.Infrastructure/Migrations/20260215225818_AddIndexesForBreedAndColor.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AgroLink.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIndexesForBreedAndColor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Animals_Breed",
+                table: "Animals",
+                column: "Breed");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Animals_Color",
+                table: "Animals",
+                column: "Color");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Animals_Breed",
+                table: "Animals");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Animals_Color",
+                table: "Animals");
+        }
+    }
+}
